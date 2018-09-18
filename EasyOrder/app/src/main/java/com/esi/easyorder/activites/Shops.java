fe6150c3f87503fac9388890a5f3f6fe38f2ffe2
EslamIsmael <42698535+EslamIsmael@users.noreply.github.com>
2018-09-18 17:21:27 +0200

    merchantapp

---
+++ EasyOrder/app/src/main/java/com/esi/easyorder/activites/Shops.java
@@ -0,0 +1,18 @@
+package com.esi.easyorder.activites;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.esi.easyorder.R;
+
+public class Shops extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops);
+
+        }
+}
